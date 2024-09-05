1ee046df4ce3f09908b93dc8177cb3a50e393f48
mirazfaruk <mirazfaruk@gmail.com>
2024-09-05 18:36:19 +0600

    Added find all spots and single spot by id when click the view details button

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7plli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,15 +25,30 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const spotCollection = client.db('spotDB').collection('spot')
+        const spotCollection = client.db('spotDB').collection('spot');
+
+        // Get all tourists spots
+        app.get('/add-tourists-spot', async (req, res) => {
+            const cursor = spotCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Get specific spot using id
+        app.get('/add-tourists-spot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotCollection.findOne(query);
+            res.send(result);
+        });
 
         // POST
-        app.post('/add-tourist-spot', async (req, res) => {
+        app.post('/add-tourists-spot', async (req, res) => {
             const newSpot = req.body;
             console.log(newSpot);
             const result = await spotCollection.insertOne(newSpot);
             res.send(result);
-        })
+        });
 
 
 
