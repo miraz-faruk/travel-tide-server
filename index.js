8f2dd0578e3cddcae0761cc4bc133a36997fe49b
mirazfaruk <mirazfaruk@gmail.com>
2024-09-07 01:17:30 +0600

    complete

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-// For teacher who check the assignment
-// DB_USER=travelTide
-// DB_PASS=N706jqWcgRo9aPTn
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7plli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7plli.mongodb.net/spotDB?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
