34dc82c45f3eec5fa9093d40a321b5ed73170365
mirazfaruk <mirazfaruk@gmail.com>
2024-09-07 14:23:43 +0600

    update cors configuration

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5001;
 
-// middleware
-app.use(cors());
+
+// Configure CORS options
+const corsOptions = {
+    origin: ['http://localhost:5173/', 'https://your-firebase-deployed-site-link'], // Add your local and deployed client URLs
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7plli.mongodb.net/spotDB?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
